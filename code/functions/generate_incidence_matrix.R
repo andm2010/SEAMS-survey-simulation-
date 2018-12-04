b46215940dd2bedb5d22515dd51c3629fd45caae
Andrew Maher <andm2010@gmail.com>
2018-12-04 14:15:18 +0200

    first commit of roxygen documentation for functions assigned to Andrew

--- code/functions/generate_incidence_matrix.R
+++ code/functions/generate_incidence_matrix.R
@@ -1,30 +1,34 @@
-# generate_incidence_matrix
+#' generate_incidence_matrix is a function that returns a matrix of probabilities of infection accross indicated ranges of age and time
+#' 
+#' @param age_step numeric, indicates time at which the incidence rate is desired
+#' @param birth_dates ?
+#' @param generate_incidence a user defined or package default function which takes as arguments age and time and returns a numberic rate of incidence
+#' @return a matrix of column length \code{age_step} and row length \code{birth_dates} . 
+#' Values in matrix are numeric from 0-1, which represent the probability of becoming infected at \code{t} and \code{age_step}
+#' @examples To be entered
+#' 
 
 generate_incidence_matrix <- function(age_step,
                                       birth_dates, 
-                                      incidence_func
-){
+                                      generate_incidence)
+  {
   
-  
-  #function calculates the attrition rates i.e. prob of not getting 
-  #infected and the probability of not dying for a specific age and time.  
-  
-  infection_matrix  = matrix(NA, nrow = length(birth_dates) + age_step, ncol =  length(1:age_step))
+  incidence_matrix  = matrix(NA, nrow = length(birth_dates) + age_step, ncol =  length(1:age_step))
   times  = 0:length(birth_dates)
   
   for (aa in 1:age_step){
     
-    infection_matrix[times + aa, aa] =  incidence_func(times + aa, aa)
+    incidence_matrix[times + aa, aa] =  generate_incidence(times + aa, aa)
     
   }
   
-  return(infection_matrix)
-  
+  return(incidence_matrix)
 }
 
+# Create .Rd document 
+devtools::document()
 
+# Example, doesn't currently work 
+y <-  generate_incidence_matrix (age_step = 2,
+                                birth_dates = 10:20, generate_incidence)
 
-# test of the  function
-x = generate_incidence_matrix(2,
-                          0:5,
-                          incidence_func = Incidence_var_a)
--- code/functions/master/package_functions_workflow.R
+++ code/functions/master/package_functions_workflow.R
@@ -13,20 +13,29 @@ do_simiulation <- function (births = constant_births, # as a function
                     time_step = 1, # as a value
                     birth_dates = c(date1, date2),
                     perinatal_positivity = 0) # as vector of values, defaults to zero
-                      {
+  {
 
 incidence_matrix <- generate_incidence_matrix(birth_dates, age_max, incidence, time_step)
+
 base_mortality_matrix <-  generate_base_mortality (birth_dates, age_max, base_mortality, time_step)
+
 susceptible_survival_rate_matrix <- generate_susceptible_surv_rate(incidence_matrix, base_mortality_matrix)
+
 susceptible_cumulative_survival_matrix <-  generate_susceptible_cumulative_survival(susceptible_survival_rate_matrix)
+
 birth_counts <- generate_birth_counts(birth_scheme = births, time_step = time_step, birth_date_range = birth_dates)
+
 negative_births <- birth_counts * (1- perinatal_positivity) # find home
+
 positive_births <- birth_counts * perinatal_positivity # find home
-susceptible_pop_counts <- generate_susceptibles(survival_matrix=susceptible_cumulative_survival_matrix,
-                                                births=negative_births)
-infected_pop_counts [,,0] <- generate_infected(incidence_matrix,susceptible_pop_counts,susceptible_survival_rate_matrix) 
+
+susceptible_pop_counts <- generate_susceptibles(survival_matrix=susceptible_cumulative_survival_matrix,births=negative_births)
+
+infected_tau_0_counts <- generate_infected_tau_0(incidence_matrix,susceptible_pop_counts,susceptible_survival_rate_matrix) 
+
 infected_mortality_matrix <- generate_infected_mortality (base_mortality, excess_mortality, birth_dates, age_max) # excess + baseline mortality 
-infected_pop_counts <- generate_infected_population (infected_pop_counts,infected_mortality_matrix)
+
+infected_pop_counts <- generate_infected_population (infected_tau_0_counts,infected_mortality_matrix)
 
 }
 
